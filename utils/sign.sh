e3e85ef2b54aca3cee82269e956a2d98dccfd98b
William Elwood <w.elwood08@gmail.com>
2019-11-12 13:48:10 +0000

    Refactor build script
    Removes all the repetition that could be prone to unintentionally getting out of sync and instead expresses the minor differences as code paths.

--- utils/sign.sh
+++ utils/sign.sh
@@ -20,5 +20,5 @@ cd -
 minisign -v
 echo '#' >/tmp/minisign.key
 echo "$MINISIGN_SK" >>/tmp/minisign.key
-cd dnscrypt-proxy
+cd build
 echo | minisign -s /tmp/minisign.key -Sm dnscrypt-proxy-*.tar.gz dnscrypt-proxy-*.zip
